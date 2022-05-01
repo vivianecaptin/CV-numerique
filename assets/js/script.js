129363b52ae08967201a44ab76e7351c732722b9
viviane <vivianecaptin@gmail.com>
2022-05-01 12:14:26 +0200

    change color navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,46 @@ imgGhostNav1.addEventListener('mouseout', function() {
     imgGhostNav1.style.display = 'none';
     imgGhostNav2.style.display = 'block';
 })
+
+//navbar color
+var meElement = document.querySelector('#me');
+var skillsElement = document.querySelector('#skills');
+var expElement = document.querySelector('#experience');
+var educElement = document.querySelector('#education');
+var aboutElement = document.querySelector('#about');
+
+var navbarElement = document.querySelector('#navbar');
+
+window.addEventListener('scroll', function() {
+    var mePosition = meElement.offsetTop + meElement.offsetHeight;
+    var skillsPosition = meElement.offsetTop + meElement.offsetHeight - navbarElement.offsetHeight;
+    var expPosition = skillsElement.offsetTop + skillsElement.offsetHeight - navbarElement.offsetHeight;
+    var educPosition = expElement.offsetTop + expElement.offsetHeight - navbarElement.offsetHeight;
+    var aboutPosition = educElement.offsetTop + educElement.offsetHeight - navbarElement.offsetHeight;;
+    
+    if (window.scrollY < mePosition) {
+        navbarElement.style.backgroundColor = '#A6CDCF';
+        imgGhostNav2.src = 'assets/img/ghost_green2.png'
+        imgGhostNav1.src = 'assets/img/ghost_green.png'
+    }
+    if (window.scrollY > skillsPosition) {
+        navbarElement.style.backgroundColor = '#FFD4D0';
+        imgGhostNav2.src = 'assets/img/ghost_red2.png'
+        imgGhostNav1.src = 'assets/img/ghost_red.png'
+    }
+    if (window.scrollY > expPosition) {
+        navbarElement.style.backgroundColor = '#E4F0F1';
+        imgGhostNav2.src = 'assets/img/ghost_blue2.png'
+        imgGhostNav1.src = 'assets/img/ghost_blue.png'
+    }
+    if (window.scrollY > educPosition) {
+        navbarElement.style.backgroundColor = '#5E87BB';
+        imgGhostNav2.src = 'assets/img/ghost_light_green2.png'
+        imgGhostNav1.src = 'assets/img/ghost_light_green.png'
+    }
+    if (window.scrollY > aboutPosition) {
+        navbarElement.style.backgroundColor = '#E4F0F1';
+        imgGhostNav2.src = 'assets/img/ghost_green2.png'
+        imgGhostNav1.src = 'assets/img/ghost_green.png'
+    }
+})
